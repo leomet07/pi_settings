7c57a147f0cd3d44c52998a61f0cf679441ba4d5
leomet07 <leomet07@gmail.com>
2021-06-17 21:23:22 -0400

    Added the start of a questionare.

--- action.py
+++ action.py
@@ -10,8 +10,8 @@ def create_wpa_supplicant_text(network_name : str, network_password : str):
 	sensitive = (template.replace("NETWORK_NAME", network_name)).replace("NETWORK_PASSWORD", network_password)
 	
 	return sensitive
-def create_wpa_supplicant_file(path : str, sensitive : str):
-	with open(path, "w") as file:
+def create_wpa_supplicant_file(pathfile : str, sensitive : str):
+	with open(pathfile, "w") as file:
 		file.write(sensitive)
 		
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -8,6 +8,16 @@ if len(sys.argv) == 1:
 	print("Submit a directory please!")
 	exit(1)
 
-print(sys.argv[1])
+bootdir = sys.argv[1]
 
-create_wpa_supplicant_file(os.path.join(sys.argv[1], "wpa_supplicant.conf"), create_wpa_supplicant_text(os.getenv("NETWORK_NAME"), os.getenv("NETWORK_PASSWORD")))
+if input("Do you want to create a wpa_supplicant.conf? Press Y if yes: ").lower() == "y":
+	network_name = os.getenv("NETWORK_NAME")
+	network_password = os.getenv("NETWORK_PASSWORD")
+	sensitive = create_wpa_supplicant_text(os.getenv("NETWORK_NAME"), os.getenv("NETWORK_PASSWORD"))
+
+	filepath = os.path.join(bootdir, "wpa_supplicant.conf")
+	
+	create_wpa_supplicant_file(filepath, sensitive )
+	print("Created a wpa_supplicant.conf in ", bootdir)
+else:
+	print("Did not create a wpa_supplicant.conf ")
